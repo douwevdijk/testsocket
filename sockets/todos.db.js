ef7402481de238a86a427ab17f1a7f6986c135a8
douwevdijk <douwevdijk@hotmail.com>
2015-08-23 08:58:02 +0200

    new

--- sockets/todos.db.js
+++ sockets/todos.db.js
@@ -1,26 +1,350 @@
 
-var mongojs = require('mongojs');
-var db = mongojs('douwevdijk:willem901@ds039271.mongolab.com:39271/filter', ['user']);
+var r = require('rethinkdb');
+var _und = require('underscore');
+var q = require('q');
 
-var todos = {
+var connection = null;
 
-    getAllUsers: function(callback) {
-        db.user.find(callback);
-    },
-    addUser: function(todo, callback) {
-        db.user.insert(todo, callback);
-    },
-    updateTodo: function(todo, callback) {
-        db.todos.update({
-            id: todo.id
-        }, todo, {}, callback);
-    },
-    deleteTodo: function(id, callback) {
-        db.todos.remove({
-            id: id
-        }, '', callback);
-
+var dbConfig = {
+    host: process.env.RDB_HOST || '127.0.0.2',
+    port: parseInt(process.env.RDB_PORT) || 28015,
+    db  : 'voting',
+    tables: {
+        'questions': 'id',
+        'users': 'id',
+        'results': 'id'
     }
+};
+
+module.exports = function() {
+
+    this.connect = function () {
+        var deferred = q.defer();
+
+        r.connect( { host: dbConfig.host, port: dbConfig.port, db: 'voting' }, function(err, conn) {
+            if (err) {
+                //
+            }
+            connection = conn;
+            deferred.resolve();
+        });
+
+        return deferred.promise;
+
+    };
+
+    this.getstreamQuestions = function (callback) {
+        r.table('questions').get('q').changes().run(connection, function(err, cursor) {
+            if (err) throw err;
+            cursor.each(function(err, row) {
+                if (err) { callback(null); } else {
+                    callback(row);
+                }
+            });
+        });
+    };
+
+    this.getStreamResults = function (callback) {
+        r.table('results').filter(function (doc) {
+            return doc('active').eq(true)
+        }).changes({squash: true}).run(connection, function (err, cursor) {
+            if (err) throw err;
+            cursor.each(function (err, row) {
+                if (err || !row.new_val) {
+                    return;
+                } else {
+                    return callback(row);
+                }
+            });
+        });
+    };
+
+    this.getStreamScore = function (callback) {
+        r.table('score').get('score').changes().run(connection, function (err, cursor) {
+            if (err) throw err;
+            cursor.each(function (err, row) {
+                if (err) callback(null);
+                return callback(row);
+            });
+        });
+    };
+
+    this.getInitialData_results = function (callback) {
+        r.table('results').run(connection, function (err, data) {
+            if (err) {
+                return callback(null);
+            } else {
+                return callback(data.toArray());
+            }
+        });
+    }
+
+    this.addUser = function (callback) {
+        r.uuid().run(connection, function (err, id) {
+
+            r.table('users').get('u').update({
+                users: r.row('users').append({id: id})
+            }).run(connection, function (err, cursor) {
+                if (err) {
+                    return callback(null);
+                } else {
+                    return callback({key: id});
+                }
+            })
+
+        });
+    };
+
+    this.getResultById = function (id, callback) {
+        r.table('results').get(id).run(connection, function (err, data) {
+            if (err) {
+                return callback(null);
+            } else {
+                return callback(data);
+            }
+        });
+    };
+
+    this.getResult = function (payload, callback) {
+
+        qId = payload.qId;
+        pId = payload.pId;
+
+        r.table('results').get(qId)('results')
+            .filter(function (item) {
+                return item('pId').eq(pId)
+            })
+            .run(connection, function (err, data) {
+                if (err) {
+                    return callback(null);
+                } else {
+                    return callback(data);
+                }
+            })
+
+    };
+
+    this.addQuestion = function (callback) {
+        r.uuid().run(connection, function (err, id) {
+            r.table('questions').update({
+                'questions': r.row('questions').append({id: id, active: false, questions: [], type: 'multi'})
+            }).run(connection, function (err, cursor) {
+                if (err) {
+                    return callback(null);
+                } else {
+                    var obj = {};
+                    obj.id = id;
+                    obj.active = false;
+                    obj.results = [];
+
+                    //add to results
+                    r.table('results').insert(obj).run(connection, function (err, cb) {
+                        return callback({result: 'OK'});
+                    })
+
+                }
+            })
+        });
+    };
+
+    this.deleteQuestion = function (payload, callback) {
+
+        var id = payload.id;
+        var idx = payload.idx;
+
+        r.table('questions').get('q')
+            .update({'questions': r.row('questions').deleteAt(idx)})
+            .run(connection, function (err, cb) {
+                if (err) {
+                    return callback(null);
+                } else {
+                    r.table('results').get(id).delete().run(connection, function (err, cb) {
+                        return callback({result: 'OK'});
+                    })
+                }
+            });
+
+    };
+
+    this.resetQuestions = function (callback) {
+        r.table('questions')
+            .update({
+                'questions': r.row('questions').map(function (doc) {
+                    return doc.merge({active: false})
+                })
+            })
+            .run(connection).then(function () {
+                r.table('results').update({active: false}).run(connection, function () {
+                    callback({results: 'OK'});
+                })
+            }).error(function (err) {
+                callback(null);
+            })
+    };
+
+    this.updateActive = function (payload, callback) {
+
+        var id = payload.id;
+        var b = payload.b;
+
+        r.table('questions').get('q')
+            .update({
+                'questions': r.row('questions').map(function (doc) {
+                    return r.branch(doc('id').eq(id), doc.merge({active: b}), doc.merge({active: false}))
+                })
+            }).run(connection).then(function () {
+            r.table('results').update({active: r.branch(r.row('id').eq(id), b, false)}).run(connection, function (err, cb) {
+                return callback({result: 'OK'});
+            })
+        }).error(function (err) {
+                return callback(null);
+            })
+    };
+
+    this.updateMulti = function (obj, callback) {
+
+        r.table('results').get('r')
+            .update({results: arr})
+            .run(connection)
+            .then(function () {
+                return callback({result: 'OK'});
+            }).error(function (err) {
+                return callback(null);
+            })
+
+    };
+
+    this.updateArr = function (arr, callback) {
+
+        r.table('questions').get('q')
+            .update({questions: arr})
+            .run(connection)
+            .then(function () {
+                return callback({result: 'OK'});
+            }).error(function (err) {
+                return callback(null);
+            })
+
+    };
+
+    this.updateQ = function (payload, callback) {
+
+        var idx = payload.idx;
+
+        delete payload.idx;
+
+        r.table('questions').get('q')
+            .update({
+                'questions': r.row('questions').changeAt(idx, r.row('questions').nth(idx).merge(payload))
+            }).run(connection).then(function () {
+            return callback({result: 'OK'});
+        }).error(function (err) {
+                return callback(null);
+            })
+    };
+
+    this.updateMulti = function (payload, callback) {
+
+        //first get index of current user
+
+        var qId = payload.qId;
+        var pId = payload.player;
+
+        r.table('results').get(qId).run(connection, function (err, data) {
+            var idx = checkPlayerexists(pId, data.results);
+
+            if (idx === -1) {
+                r.table('results').get(qId)
+                    .update({
+                        'results': r.row('results').append({
+                            pId: pId,
+                            answer: payload.answer,
+                            correct: payload.correct,
+                            cnt: payload.int
+                        })
+                    })
+                    .run(connection, function (err, cb) {
+                        if (err) {
+                            callback(null);
+                        } else {
+                            callback({result: 'OK'})
+                        }
+                    })
+            } else {
+                r.table('results').get(qId)
+                    .update({
+                        'results': r.row('results').changeAt(idx, {
+                            pId: pId,
+                            answer: payload.answer,
+                            correct: payload.correct,
+                            cnt: payload.int
+                        })
+                    })
+                    .run(connection, function (err, cb) {
+                        if (err) {
+                            callback(null);
+                        } else {
+                            callback({result: 'OK'})
+                        }
+                    })
+            }
+
+        });
+
+    };
+
+    this.updateOpen = function (payload, callback) {
+
+        var qId = payload.qId;
+        var pId = payload.player;
+
+        r.table('results').get(qId).run(connection, function (err, data) {
+
+            r.table('results').get(qId)
+                .update({'results': r.row('results').append({pId: pId, text: payload.text})})
+                .run(connection, function (err, cb) {
+                    if (err) {
+                        callback(null);
+                    } else {
+                        callback({result: 'OK'})
+                    }
+                });
+        })
+
+    };
+
+    this.getScore = function (callback) {
+        r.table('results')
+            .concatMap(function (s) {
+                return s('results')
+            })
+            .hasFields('correct', 'cnt')
+            .map(function (m) {
+                return r.branch(m('correct'), m.merge({pts: 1}), m.merge({pts: 0}))
+            })
+            .group('pId').map(function (gr) {
+                return {pts: gr('pts'), cnt: gr('cnt')}
+            })
+            .reduce(function (left, right) {
+                return {pts: left('pts').add(right('pts')), cnt: left('cnt').add(right('cnt'))}
+            })
+            .ungroup().orderBy(r.desc(r.row('reduction')('pts')))
+            .run(connection, function (err, data) {
+                if (err) {
+                    callback(null);
+                } else {
+                    r.table('score').update({score: data}).run(connection, function () {
+                        callback(data);
+                    })
+                }
+            });
+    };
+
 }
 
-module.exports = todos;
+
+//general functions
+
+function checkPlayerexists(id, arr) {
+    return _und.indexOf(_und.pluck(arr, 'pId'), id);
+}
